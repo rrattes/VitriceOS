e7cf831831e2ab61d0a50a52863f1a77fb0ad830
rrattes <166952707+rrattes@users.noreply.github.com>
2026-04-11 15:35:59 -0300

    Update archiso boot modes to non-deprecated values

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -8,9 +8,7 @@ iso_application="VitriceOS Live ISO"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.systemd-boot.esp' 'uefi-x64.systemd-boot.esp'
-           'uefi-ia32.systemd-boot.eltorito' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux' 'uefi.systemd-boot')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
@@ -21,6 +19,8 @@ file_permissions=(
   ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
   ["/usr/local/bin/vitrice-install"]="0:0:755"
+  ["/usr/local/bin/vitrice-autoinstall-launcher"]="0:0:755"
+  ["/etc/systemd/system/vitrice-autoinstall.service"]="0:0:644"
   ["/usr/local/lib/vitrice-installer/common.sh"]="0:0:755"
   ["/usr/local/lib/vitrice-installer/steps/00-preflight.sh"]="0:0:755"
   ["/usr/local/lib/vitrice-installer/steps/10-partition.sh"]="0:0:755"
