650a81da084fa1fbd29988939fd2ac9ae14b5e25
rrattes <166952707+rrattes@users.noreply.github.com>
2026-04-11 19:14:48 -0300

    Simplify chroot locale setup and explicitly unlock accounts

--- releng/airootfs/usr/local/lib/vitrice-installer/steps/20-bootstrap.sh
+++ releng/airootfs/usr/local/lib/vitrice-installer/steps/20-bootstrap.sh
@@ -4,14 +4,14 @@ source /usr/local/lib/vitrice-installer/common.sh
 log "Bootstrap do sistema base"
 
 run "pacstrap -K '${VITRICE_TARGET}' base linux linux-firmware sudo networkmanager grub efibootmgr"
-run "genfstab -U '${VITRICE_TARGET}' >> '${VITRICE_TARGET}/etc/fstab'"
+run "genfstab -U '${VITRICE_TARGET}' > '${VITRICE_TARGET}/etc/fstab'"
 
 cat > "${VITRICE_TARGET}/root/vitrice-post-chroot.sh" <<CHROOT
 #!/usr/bin/env bash
 set -euo pipefail
 ln -sf /usr/share/zoneinfo/${VITRICE_TIMEZONE} /etc/localtime
 hwclock --systohc
-sed -i 's/^#${VITRICE_LOCALE}/${VITRICE_LOCALE}/' /etc/locale.gen
+sed -i "s/^#${VITRICE_LOCALE}/${VITRICE_LOCALE}/" /etc/locale.gen
 locale-gen
 echo 'LANG=${VITRICE_LOCALE}' > /etc/locale.conf
 echo 'KEYMAP=${VITRICE_KEYMAP}' > /etc/vconsole.conf
@@ -23,6 +23,10 @@ cat > /etc/hosts <<HOSTS
 HOSTS
 systemctl enable NetworkManager
 useradd -m -G wheel -s /bin/bash '${VITRICE_USERNAME}' || true
+echo 'root:${VITRICE_ROOT_PASSWORD}' | chpasswd
+echo '${VITRICE_USERNAME}:${VITRICE_USER_PASSWORD}' | chpasswd
+usermod -U root || true
+usermod -U '${VITRICE_USERNAME}' || true
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=VitriceOS
 grub-mkconfig -o /boot/grub/grub.cfg
